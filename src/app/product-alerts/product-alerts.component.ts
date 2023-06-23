d470febc82929cf709e243acf5779fa6dc75c27d
Eike Souza <eike2528@hotmail.com>
2023-06-23 20:19:37 -0300

    pass data to a parent component

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'; // Para receber os dados do produto vamos ter que importar o "Input"
+import { Component, Input, Output, EventEmitter } from '@angular/core'; // Para receber os dados do produto vamos ter que importar o "Input"
 import { Product } from '../products';// Importando o objeto/arquivo que possui meus produtos
 
 @Component({ // Aqui eu declaro que a classe a seguir se trata de um componente. E também informa os Metadados deste componente.
@@ -9,4 +9,5 @@ import { Product } from '../products';// Importando o objeto/arquivo que possui 
 
 export class ProductAlertsComponent { // também exportamos a classe que irá manipular as funcionalidades do componente
   @Input() product: Product | undefined; // O decorator "@input()" indica que a propriedade do valor (product) pode receber um valor a partir de um componente pai "Product", ou será vazio
+  @Output() notify = new EventEmitter(); // Configurando com o decorator "@Output" permitirá o component emitir um evento quando o valor de "notify", for alterado
 }
