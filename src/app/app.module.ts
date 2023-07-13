f8710285540c4a27d18ee2297782728dc33ed6c3
Eike Souza <eike2528@hotmail.com>
2023-07-13 20:31:29 -0300

    Managing Data - using HTTP data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { RouterModule, RouterLink } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './UNUSEDapp-routing.module';
 import { AppComponent } from './app.component';
@@ -10,19 +11,22 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
-      {path: 'cart', component: CartComponent}
+      {path: 'cart', component: CartComponent},
+      {path: 'shipping', component: ShippingComponent}
     ]),
     BrowserModule,
     ReactiveFormsModule,
     AppRoutingModule,
     RouterModule,
-    RouterLink
+    RouterLink,
+    HttpClientModule
   ],
   declarations: [ // Aqui preciso declarar todas minhas classes exportadas, ou seja, meus components que são fruto dessa classe.
     AppComponent,
@@ -30,7 +34,8 @@ import { CartComponent } from './cart/cart.component';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    ShippingComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { Product } from './products';
 
@@ -9,7 +10,9 @@ import { Product } from './products';
 export class CartService {
   items: Product[] = [];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient // injetando o módulo httpClient dentro do meu módulo
+  ) { }
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -22,5 +25,11 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  // usaremos o metodo geT() do httpModule para receber os dados do nosso shipping.json, que simula dados vindos de uma API
+  // -> <> -> defino os atributos e seus tipos, para ser compatível com o que é necessário para suprir o objeto/Interface
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('assets/shipping.json')
   }
 }
